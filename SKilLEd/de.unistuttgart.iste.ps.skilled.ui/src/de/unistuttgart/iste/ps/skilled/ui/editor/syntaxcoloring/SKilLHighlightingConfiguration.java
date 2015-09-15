dea03acd26ce5f1286272ab09bc821e67032e975
Ken Singer <Ken.Singer@gmx.de>
2015-09-15 12:09:08 +0200

    Added defaut colors for Syntaxhighlighting. #77

--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/editor/syntaxcoloring/SKilLHighlightingConfiguration.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/editor/syntaxcoloring/SKilLHighlightingConfiguration.java
@@ -1,6 +1,7 @@
 package de.unistuttgart.iste.ps.skilled.ui.editor.syntaxcoloring;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
@@ -21,7 +22,6 @@ public class SKilLHighlightingConfiguration extends DefaultHighlightingConfigura
     public static final String ENUM_ID = "enum";
     public static final String TYPEDEF_ID = "typedef";
     public static final String BUILDINTYPE_ID = "buildintype";
-    public static final String CROSS_REF = "crossreference";
 
     @Override
     public void configure(IHighlightingConfigurationAcceptor acceptor) {
@@ -31,49 +31,48 @@ public class SKilLHighlightingConfiguration extends DefaultHighlightingConfigura
         acceptor.acceptDefaultHighlighting(INTERFACE_ID, "Interface", interfaceTextStyle());
         acceptor.acceptDefaultHighlighting(ENUM_ID, "Enumeration", enumTextStyle());
         acceptor.acceptDefaultHighlighting(TYPEDEF_ID, "Typedef", typedefTextStyle());
-        acceptor.acceptDefaultHighlighting(CROSS_REF, "Cross-References", crossReferenceTextStyle());
         acceptor.acceptDefaultHighlighting(BUILDINTYPE_ID, "Build In Type", buildintypeTextStyle());
-    }
-
-    public static TextStyle crossReferenceTextStyle() {
-        TextStyle textStyle = new TextStyle();
-        textStyle.setStyle(SWT.ITALIC);
-        return textStyle;
     }
 
     public static TextStyle usertypeTextStyle() {
         TextStyle textStyle = new TextStyle();
         textStyle.setStyle(SWT.ITALIC);
+        textStyle.setColor(new RGB(0, 0, 0));
         return textStyle;
     }
 
     public static TextStyle interfaceTextStyle() {
         TextStyle textStyle = new TextStyle();
         textStyle.setStyle(SWT.ITALIC);
+        textStyle.setColor(new RGB(50, 63, 112));
         return textStyle;
     }
 
     public static TextStyle enumTextStyle() {
         TextStyle textStyle = new TextStyle();
         textStyle.setStyle(SWT.ITALIC);
+        textStyle.setColor(new RGB(100, 70, 50));
         return textStyle;
     }
-    
+
     public static TextStyle typedefTextStyle() {
         TextStyle textStyle = new TextStyle();
         textStyle.setStyle(SWT.ITALIC);
+        textStyle.setColor(new RGB(0, 0, 0));
         return textStyle;
     }
 
     public static TextStyle headcommentTextStyle() {
         TextStyle textStyle = new TextStyle();
         textStyle.setStyle(SWT.ITALIC);
+        textStyle.setColor(new RGB(241, 51, 127));
         return textStyle;
     }
 
     public static TextStyle buildintypeTextStyle() {
         TextStyle textStyle = new TextStyle();
         textStyle.setStyle(SWT.ITALIC);
+        textStyle.setColor(new RGB(127, 0, 85));
         return textStyle;
     }
 }
