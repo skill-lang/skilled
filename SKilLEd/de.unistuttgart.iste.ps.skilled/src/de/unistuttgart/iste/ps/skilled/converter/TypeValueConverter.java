8fbdcca5d28a3eafe516bdd437df1b374cb17cb3
Tobias Heck <tobias.heck@ymail.com>
2015-11-11 13:58:25 +0100

    Fixed typename inconsistencies with underscores and typenames being case sensitive. #114

--- SKilLEd/de.unistuttgart.iste.ps.skilled/src/de/unistuttgart/iste/ps/skilled/converter/TypeValueConverter.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled/src/de/unistuttgart/iste/ps/skilled/converter/TypeValueConverter.java
@@ -19,10 +19,33 @@ public class TypeValueConverter extends AbstractLexerBasedConverter<String> {
         if (Strings.isEmpty(string))
             throw new ValueConverterException("Couldn't convert empty string.", node, null);
         try {
-        	return string.replace("_", "");
+        	string = string.toLowerCase();
+        	int index = 0;
+        	while (string.charAt(index) == '_') {
+        		index++;
+        		if (index == string.length()) return string;
+        	}
+        	boolean wasUnderscore = true;
+        	while (index < string.length()) {
+        		if (string.charAt(index) != '_') {
+        			index++;
+        			wasUnderscore = false;
+        			continue;
+        		}
+        		if (wasUnderscore == false) {
+        			wasUnderscore = true;
+        			string = string.substring(0,index) + string.substring(index + 1);
+        			continue;
+        		}
+        		index++;
+        	}
+        	if (!string.matches("_*")) {
+        		while (string.charAt(string.length() - 1) == '_')
+        			string = string.substring(0, string.length() - 1);
+        	}
+        	return string;
         } catch (Exception e) {
             throw new ValueConverterException("Couldn't convert '" + string + "'.", node, e);
         }
 	}
-
 }
