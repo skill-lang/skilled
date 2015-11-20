feae30168da855ae6067619072528e3d0edc0e35
Jan Berberich <berberjn@stud.informatik.uni-stuttgart.de>
2015-11-20 19:16:28 +0100

    Mouseover documentation now working #122

--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/mouseoverdoc/SKilLObjectDocumentationProvider.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/mouseoverdoc/SKilLObjectDocumentationProvider.java
@@ -2,12 +2,83 @@ package de.unistuttgart.iste.ps.skilled.ui.mouseoverdoc;
  
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.documentation.IEObjectDocumentationProvider;
+import org.eclipse.xtext.documentation.impl.MultiLineCommentDocumentationProvider;
 
+import de.unistuttgart.iste.ps.skilled.sKilL.Enumtype;
+import de.unistuttgart.iste.ps.skilled.sKilL.Restriction;
+import de.unistuttgart.iste.ps.skilled.sKilL.TypeDeclaration;
+import de.unistuttgart.iste.ps.skilled.sKilL.Typedef;
+import de.unistuttgart.iste.ps.skilled.sKilL.Usertype;
+/**
+ * This Class defines the Object Documentation Provider for mouseover documentation in SKilL and 
+ * creates the object documentation string for the documentation
+ * 
+ * @author Jan Berberich
+ */
 public class SKilLObjectDocumentationProvider implements IEObjectDocumentationProvider {
- 
-    @Override
-    public String getDocumentation(EObject o) {
-        return "Test2" + o.toString();
+	
+	@Override
+	public String getDocumentation(EObject o) {
+		String returnString = "";
+    	//binder.bind(String.class).annotatedWith(com.google.inject.name.Names.named((XtextContentAssistProcessor.COMPLETION_AUTO_ACTIVATION_CHARS))).toInstance(" \t" + eol);
+    	if(o instanceof TypeDeclaration){
+    		TypeDeclaration td = (TypeDeclaration)o;
+    		if(td.getComment()!= null){
+    			returnString = removeCommentMarkers(td.getComment());
+    		}
+    		if(td instanceof Usertype){
+    			Usertype ut =(Usertype)td;
+    			if(ut.getRestrictions()!= null){
+    				returnString = returnString + "\n"+"<b>Restrictions: </b>";
+    			}
+    			for(Restriction r: ut.getRestrictions()){
+    				returnString = returnString + r.getRestrictionName();
+    			}
+    			
+    		}
+    		returnString = returnString;
+    		return returnString;
+    	}else if(o instanceof Typedef){
+    		Typedef td = (Typedef) o;
+    		if(td.getComment()!= null){
+    			returnString = removeCommentMarkers(td.getComment());
+    		}
+    		if(td instanceof Usertype){
+    			Usertype ut =(Usertype)td;
+    			if(ut.getRestrictions()!= null){
+    				returnString = returnString + "\n"+  "<b>Restrictions: </b>";
+    			}
+    			
+    			for(Restriction r: ut.getRestrictions()){
+    				returnString = returnString + r.getRestrictionName();
+    			}
+    			
+    		}
+    		return returnString;
+    	}else if(o instanceof Enumtype){
+    		Enumtype en = (Enumtype)o;
+    		if(en.getComment()!= null){
+    			returnString = removeCommentMarkers(en.getComment());
+    		}
+    	}
+        return returnString;
     }
- 
+    
+    /**
+     * Removes the comment markers from a comment
+     * @param s The comment
+     * @return The text of the comment
+     */
+    String removeCommentMarkers(String s){
+    	s = s.substring(2);
+		s = s.substring(0, s.length()-2);
+    	for(int i = 0; i<s.length(); i++){
+    		//Replace "*" with "\n"
+    		if(s.substring(i,i+1).equals("*")){
+    			s = s.substring(0, i)+""+ s.substring(i+1, s.length());
+    		}
+    	}
+		return s;
+    }
 }
+
--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/mouseoverdoc/SKilLObjectHoverProvider.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/mouseoverdoc/SKilLObjectHoverProvider.java
@@ -4,10 +4,39 @@ package de.unistuttgart.iste.ps.skilled.ui.mouseoverdoc;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.ui.editor.hover.html.DefaultEObjectHoverProvider;
 
+import de.unistuttgart.iste.ps.skilled.sKilL.Enumtype;
+import de.unistuttgart.iste.ps.skilled.sKilL.Interfacetype;
+import de.unistuttgart.iste.ps.skilled.sKilL.TypeDeclaration;
+import de.unistuttgart.iste.ps.skilled.sKilL.Typedef;
+import de.unistuttgart.iste.ps.skilled.sKilL.Usertype;
+/**
+ * This Class defines the HoverProvider for mouseover documentation in SKilL and 
+ * creates the title string for the documentation
+ * 
+ * @author Jan Berberich
+ */
 public class SKilLObjectHoverProvider extends DefaultEObjectHoverProvider {
     @Override
     protected String getFirstLine(EObject o) {
-        return o.toString();
+    	String comment = "";
+    	if(o instanceof TypeDeclaration){
+    		TypeDeclaration td = (TypeDeclaration)o;
+    		if(o instanceof Usertype){
+    			comment = comment +"Type ";
+    		}
+    		if(o instanceof Interfacetype){
+    			comment = comment +"Interface ";
+    		}
+    		comment = comment + td.getName();
+    	}else if(o instanceof Typedef){
+    		Typedef td = (Typedef)o;
+    		comment = comment +"Typedef: " + td.getName();
+    	}else if(o instanceof Enumtype){
+    		Enumtype en = (Enumtype)o;
+    		comment = comment +"Enum: " + en.getName();
+    		return comment;
+    	}
+    	return comment;
     }
  
 }
