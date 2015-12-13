45f58f22d8d8f47abd2c1c569c9ae66d3cc2b0c8
Marco Link <linkmo@studi.informatik.uni-stuttgart.de>
2015-12-13 18:17:50 +0100

    Fixed Bug with not changing the default path when creating a new project. #158

--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/wizards/SKilLNewProjectWizard.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/wizards/SKilLNewProjectWizard.java
@@ -1,8 +1,18 @@
 package de.unistuttgart.iste.ps.skilled.ui.wizards;
 
+import java.io.ByteArrayInputStream;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
@@ -45,21 +55,49 @@ public class SKilLNewProjectWizard extends Wizard implements INewWizard {
     /**
      * This method is called when 'Finish' button is pressed in the wizard. We will create a new project with the given
      * informations and add the nature.
+     * 
+     * @return true if everything went fine - else false.
      */
     @Override
     public boolean performFinish() {
-        boolean result = false;
-        IProject project = mainpage.getProjectHandle();
-        try {
-            project.create(null);
-            result = true;
-        } catch (CoreException e) {
-            e.printStackTrace();
+        IProgressMonitor progressMonitor = new NullProgressMonitor();
+        IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+        IProject project = null;
+        IProjectDescription projectDesc = null;
+        // If default path is not set change to the inserted location.
+        if (!mainpage.useDefaults()) {
+            project = root.getProject(mainpage.getProjectName());
+            IWorkspace w = ResourcesPlugin.getWorkspace();
+            projectDesc = w.newProjectDescription(project.getName());
+            IPath projectLocation = mainpage.getLocationPath().append(mainpage.getProjectName());
+            projectDesc.setLocation(projectLocation);
+            try {
+                project.create(projectDesc, progressMonitor);
+            } catch (CoreException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+        } else {
+            project = mainpage.getProjectHandle();
+            try {
+                project.create(progressMonitor);
+            } catch (CoreException e) {
+                e.printStackTrace();
+                return false;
+            }
         }
 
-        IProjectDescription projectDesc = null;
         try {
             project.open(null);
+
+            // Create new .skills file for the tool support.
+            String toolsFileName = project.getFullPath().toString() + "/.skillls";
+            IFile toolsFile = root.getFile(new Path(toolsFileName));
+            if (!toolsFile.exists()) {
+                toolsFile.create(new ByteArrayInputStream(new byte[0]), false, null);
+            }
+
             projectDesc = project.getDescription();
 
             String[] natureIds = projectDesc.getNatureIds();
@@ -71,11 +109,13 @@ public class SKilLNewProjectWizard extends Wizard implements INewWizard {
                 project.setDescription(projectDesc, null);
             } catch (CoreException e2) {
                 e2.printStackTrace();
+                return false;
             }
         } catch (CoreException e1) {
             e1.printStackTrace();
+            return false;
         }
 
-        return result;
+        return true;
     }
 }
