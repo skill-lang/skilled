0f53695d9e54b7c157db29f5a6638d893aded680
Nico Rusam <nico.rusam@gmail.com>
2015-12-22 17:19:34 +0100

    Add Todos and change the call of Armins skillls.jar to a method call. #74

--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/views/FileChangeAction.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/views/FileChangeAction.java
@@ -13,6 +13,11 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 
 public class FileChangeAction {
+
+	// TODO: Je nachdem was Armin aussagt werden anstatt seine skillls.jar
+	// aufzurufen direkt seine Methoden verwendet. Diese Methoden müssen
+	// aufgerufen werden um die .skills Datei zu erzeugen.
+
 	public void save() {
 		String commandId = "org.eclipse.ui.file.save";
 		ICommandService service = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
@@ -21,48 +26,47 @@ public class FileChangeAction {
 
 			@Override
 			public void notHandled(String arg0, NotHandledException arg1) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("save notHandled");
 			}
 
 			@Override
 			public void postExecuteFailure(String arg0, ExecutionException arg1) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("save ExecuteFailure");
 			}
 
 			@Override
 			public void postExecuteSuccess(String arg0, Object arg1) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("save ExecuteSuccess");
 			}
 
 			@Override
 			public void preExecute(String arg0, ExecutionEvent arg1) {
-				Process p = null;
-				try {
-					p = Runtime.getRuntime().exec("java -jar skillls.jar -e $PATH_TO_PROJECT $PARAMS", null, new File("lib"));
-					p.waitFor();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				try (InputStreamReader reader = new InputStreamReader(p.getInputStream())) {
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("Save");
+				// Process p = null;
+				// try {
+				// p = Runtime.getRuntime().exec("java -jar skillls.jar -e
+				// $PATH_TO_PROJECT $PARAMS", null,
+				// new File("lib"));
+				// p.waitFor();
+				// } catch (IOException e) {
+				// e.printStackTrace();
+				// } catch (InterruptedException e) {
+				// e.printStackTrace();
+				// }
+				// try (InputStreamReader reader = new
+				// InputStreamReader(p.getInputStream())) {
+				// } catch (IOException e) {
+				// e.printStackTrace();
+				// }
+				String pathToProject = "C:\\Users\\nicoa\\Documents\\skilled-skilled.code2\\runtime-New_configuration\\Test";
+				String parameter = "--help";
+				System.out.println(new String[] { "-e", pathToProject, parameter });
+				de.unistuttgart.iste.ps.skillls.main.MainClass.main(new String[] { "-e", pathToProject, parameter });
+				System.out.println("save preExecute");
 			}
 
 		});
 	}
-	
+
 	public void saveAll() {
 		String commandId = "org.eclipse.ui.file.saveAll";
 		ICommandService service = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
@@ -71,31 +75,27 @@ public class FileChangeAction {
 
 			@Override
 			public void notHandled(String arg0, NotHandledException arg1) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("saveAll nothandled");
 			}
 
 			@Override
 			public void postExecuteFailure(String arg0, ExecutionException arg1) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("saveAll ExecuteFailure");
 			}
 
 			@Override
 			public void postExecuteSuccess(String arg0, Object arg1) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("saveAll ExecuteSuccess");
 			}
 
 			@Override
 			public void preExecute(String arg0, ExecutionEvent arg1) {
-				// TODO Auto-generated method stub
-				System.out.println("SaveAll");
+				System.out.println("saveAll preExecute");
 			}
 
 		});
 	}
-	
+
 	public void rename() {
 		String commandId = "org.eclipse.ui.file.import";
 		ICommandService service = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
@@ -104,26 +104,22 @@ public class FileChangeAction {
 
 			@Override
 			public void notHandled(String arg0, NotHandledException arg1) {
-				// TODO Auto-generated method stub
-				System.out.println("import1");
+				System.out.println("import nothandled");
 			}
 
 			@Override
 			public void postExecuteFailure(String arg0, ExecutionException arg1) {
-				// TODO Auto-generated method stub
-				System.out.println("import2");
+				System.out.println("import ExecuteFailure");
 			}
 
 			@Override
 			public void postExecuteSuccess(String arg0, Object arg1) {
-				// TODO Auto-generated method stub
-				System.out.println("import3");
+				System.out.println("import ExecuteSuccess");
 			}
 
 			@Override
 			public void preExecute(String arg0, ExecutionEvent arg1) {
-				// TODO Auto-generated method stub
-				System.out.println("import4");
+				System.out.println("import preExecute");
 			}
 
 		});
