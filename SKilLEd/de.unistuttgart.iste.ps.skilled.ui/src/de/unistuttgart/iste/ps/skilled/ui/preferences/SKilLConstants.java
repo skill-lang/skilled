edf99cd8d233f96657de2cc0597fdd2ab5269e47
Daniel Ryan Degutis <ryan33@t-online.de>
2015-12-28 13:53:00 +0100

    Improved preference page according to reviewer. #89

--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/preferences/SKilLConstants.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/preferences/SKilLConstants.java
@@ -1,17 +1,20 @@
 package de.unistuttgart.iste.ps.skilled.ui.preferences;
 
+/**
+ * @author Daniel Ryan Degutis
+ */
 public class SKilLConstants {
-	
-	public static final String TARGET_PROJECT_PATH = "targetprojectpath";
-	
-	public static final String GENERATE_OPTION = "generateoption";
-	
-	public static final String GENERATOR_PATH = "generatorpath";
-	
-	public static final String LANGUAGE_SELECTION = "languageselection";
-	
-	public static final String EXECUTION_ENVIRONMENT = "executionenvironment";
-	
-	public static final String MODULE_NAME = "modulename";
+
+    public static final String TARGET_PROJECT_PATH = "targetprojectpath";
+
+    public static final String GENERATE_OPTION = "generateoption";
+
+    public static final String GENERATOR_PATH = "generatorpath";
+
+    public static final String LANGUAGE_SELECTION = "languageselection";
+
+    public static final String EXECUTION_ENVIRONMENT = "executionenvironment";
+
+    public static final String MODULE_NAME = "modulename";
 
 }
--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/preferences/SKilLPreferencePage.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/preferences/SKilLPreferencePage.java
@@ -1,10 +1,8 @@
 package de.unistuttgart.iste.ps.skilled.ui.preferences;
 
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.jface.preference.RadioGroupFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
@@ -12,93 +10,101 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.xtext.ui.editor.preferences.AbstractPreferencePage;
 
-import de.unistuttgart.iste.ps.skilled.ui.internal.SKilLActivator;
 
-public class SKilLPreferencePage extends FieldEditorPreferencePage
-		implements IWorkbenchPreferencePage, IWorkbenchPropertyPage {
-	
-	private DirectoryFieldEditor projectPathField;
-	private RadioGroupFieldEditor generateOption;
-	private FileFieldEditor generatorPathField;
-	private ComboFieldEditor languageSelectionComboField;
-	private ComboFieldEditor executionEnvironmentComboField;
-	private StringFieldEditor moduleNameField;
+/**
+ * Set preferences for a skill project via this prefrence page, like generator path, generation mode, etc.
+ * 
+ * @author Daniel Ryan Degutis
+ */
+public class SKilLPreferencePage extends AbstractPreferencePage {
 
-	public SKilLPreferencePage() {
-		super(FieldEditorPreferencePage.GRID);
-		setPreferenceStore(SKilLActivator.getInstance().getPreferenceStore());
-	}
+    private DirectoryFieldEditor projectPathField;
+    private RadioGroupFieldEditor generateOption;
+    private FileFieldEditor generatorPathField;
+    private ComboFieldEditor languageSelectionComboField;
+    private ComboFieldEditor executionEnvironmentComboField;
+    private StringFieldEditor moduleNameField;
 
-	@Override
-	protected void initialize() {
-		super.initialize();
-		getFieldEditorParent();
-	}
+    public SKilLPreferencePage() {
+        super();
+    }
 
-	private void putSeperator() {
-		Label l = new Label(getFieldEditorParent(), SWT.SEPARATOR | SWT.HORIZONTAL);
-		l.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 3, 2));
-	}
-	
-	@Override
-	protected void createFieldEditors() {
-		noDefaultAndApplyButton();
-		
-		projectPathField = new DirectoryFieldEditor(SKilLConstants.TARGET_PROJECT_PATH, "Target project location:", getFieldEditorParent());
-		addField(projectPathField);
-		
-		putSeperator();
+    @Override
+    protected void initialize() {
+        super.initialize();
+        getFieldEditorParent();
+    }
 
-		generateOption = new RadioGroupFieldEditor(SKilLConstants.GENERATE_OPTION, "Generation Mode", 2, new String[][] {{"Generate all tools (-ag)", "Generate all tools (-ag)"}, {"Generate lazy (-g)", "Generate lazy (-g)"}}, getFieldEditorParent());
-		addField(generateOption);
-		
-		putSeperator();
-		
-		generatorPathField = new FileFieldEditor(SKilLConstants.GENERATOR_PATH, "Generator path:", true, 1, getFieldEditorParent());
-		addField(generatorPathField);
+    private void putSeperator() {
+        Label l = new Label(getFieldEditorParent(), SWT.SEPARATOR | SWT.HORIZONTAL);
+        l.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 3, 2));
+    }
 
-		putSeperator();
+    @Override
+    protected void createFieldEditors() {
+        noDefaultAndApplyButton();
 
-		languageSelectionComboField = new ComboFieldEditor(SKilLConstants.LANGUAGE_SELECTION, "Select language:", new String[][] { { "Ada", "Ada" }, { "C", "C" }, { "Java", "Java" }, { "Scala", "Scala" } }, getFieldEditorParent());
-		addField(languageSelectionComboField);
+        projectPathField = new DirectoryFieldEditor(SKilLConstants.TARGET_PROJECT_PATH, "Target project location:",
+                getFieldEditorParent());
+        addField(projectPathField);
 
-		putSeperator();
+        putSeperator();
 
-		executionEnvironmentComboField = new ComboFieldEditor(SKilLConstants.EXECUTION_ENVIRONMENT, "Execution environment:", new String[][] { { "Scala", "Scala" } }, getFieldEditorParent());
-		addField(executionEnvironmentComboField);
+        generateOption = new RadioGroupFieldEditor(SKilLConstants.GENERATE_OPTION, "Generation Mode", 2, new String[][] {
+                { "Generate all tools (-ag)", "Generate all tools (-ag)" }, { "Generate lazy (-g)", "Generate lazy (-g)" } },
+                getFieldEditorParent());
+        addField(generateOption);
 
-		putSeperator();
+        putSeperator();
 
-		moduleNameField = new StringFieldEditor(SKilLConstants.MODULE_NAME, "Name of module:", getFieldEditorParent());
-		addField(moduleNameField);
-	}
+        generatorPathField = new FileFieldEditor(SKilLConstants.GENERATOR_PATH, "Generator path:", true, 1,
+                getFieldEditorParent());
+        addField(generatorPathField);
 
-	@Override
-	public boolean performOk() {
-		projectPathField.store();
-		generateOption.store();
-		generatorPathField.store();
-		languageSelectionComboField.store();
-		executionEnvironmentComboField.store();
-		moduleNameField.store();
-		return super.performOk();
-	}
-	
-	@Override
-	public void init(IWorkbench workbench) {
-		// Do nothing
-	}
+        putSeperator();
 
-	@Override
-	public IAdaptable getElement() {
-		return null;
-	}
+        languageSelectionComboField = new ComboFieldEditor(SKilLConstants.LANGUAGE_SELECTION, "Select language:",
+                new String[][] { { "Ada", "Ada" }, { "C", "C" }, { "Java", "Java" }, { "Scala", "Scala" } },
+                getFieldEditorParent());
+        addField(languageSelectionComboField);
 
-	@Override
-	public void setElement(IAdaptable element) {
-		// Do nothing
-	}
+        putSeperator();
+
+        executionEnvironmentComboField = new ComboFieldEditor(SKilLConstants.EXECUTION_ENVIRONMENT, "Execution environment:",
+                new String[][] { { "Scala", "Scala" } }, getFieldEditorParent());
+        addField(executionEnvironmentComboField);
+
+        putSeperator();
+
+        moduleNameField = new StringFieldEditor(SKilLConstants.MODULE_NAME, "Name of module:", getFieldEditorParent());
+        addField(moduleNameField);
+    }
+
+    @Override
+    public boolean performOk() {
+        projectPathField.store();
+        generateOption.store();
+        generatorPathField.store();
+        languageSelectionComboField.store();
+        executionEnvironmentComboField.store();
+        moduleNameField.store();
+        return super.performOk();
+    }
+
+    @Override
+    public void init(IWorkbench workbench) {
+        // Do nothing
+    }
+
+    @Override
+    public IAdaptable getElement() {
+        return null;
+    }
+
+    @Override
+    public void setElement(IAdaptable element) {
+        // Do nothing
+    }
 }
