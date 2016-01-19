e859c4befcaec5aac9aa6fcb0c553282eebb2d57
Marco Link <linkmo@studi.informatik.uni-stuttgart.de>
2016-01-19 13:20:59 +0100

    Toggle some code of Leslie as comment. #74

--- SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/tools/export/SaveListofAllTools.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/tools/export/SaveListofAllTools.java
@@ -5,51 +5,54 @@ import java.util.List;
 
 import de.unistuttgart.iste.ps.skillls.tools.Tool;
 
-public class SaveListofAllTools {
-	static List<String> fToolNameList = null;
-	static List<String> fToolPathList = null;
-	
-	 private static SaveListofAllTools instance = null;
-	   public SaveListofAllTools() {
-	      // Exists only to defeat instantiation.
-	   }
-	   public static SaveListofAllTools getInstance() {
-	      if(instance == null) {
-	         instance = new SaveListofAllTools();
-	      }
-	      return instance;
-	   }
 
-	// Gets the names of the tools from
-	// de.unistuttgart.iste.ps.skilled.ui.views.Toolview.java
-	public void setListofAllTools(ArrayList<Tool> allToolList) {
-		// TODO
-		if (allToolList.size() > 0) {
-			fToolNameList = new ArrayList<String>();
-			for (int i = 0; i < allToolList.size(); i++) {
-				fToolNameList.add(allToolList.get(i).getName());				
-			}
-		}
-//		System.out.println("fToolNameList size is: " + fToolNameList.size());
-		System.out.println("fToolNameList: " + fToolNameList.toString());
-	}
-	
-	public void setPathofAllTools(ArrayList<String> pathList) {
-		if (pathList.size() > 0) {
-			fToolPathList = new ArrayList<String>();
-			for (int i = 0; i < pathList.size(); i++) {
-				fToolPathList.add(pathList.get(i));
-			}
-		}
-		System.out.println("fToolPathList: " + fToolPathList.toString());
-	}
-	
-	public static List<String> getToolNameList() {
-		return fToolNameList;		
-	}
-	
-	public static List<String> getToolPathList() {
-		return fToolPathList;
-	}
+public class SaveListofAllTools {
+    static List<String> fToolNameList = null;
+    static List<String> fToolPathList = null;
+
+    private static SaveListofAllTools instance = null;
+
+    public SaveListofAllTools() {
+        // Exists only to defeat instantiation.
+    }
+
+    public static SaveListofAllTools getInstance() {
+        if (instance == null) {
+            instance = new SaveListofAllTools();
+        }
+        return instance;
+    }
+
+    // Gets the names of the tools from
+    // de.unistuttgart.iste.ps.skilled.ui.views.Toolview.java
+    public void setListofAllTools(ArrayList<Tool> allToolList) {
+        // TODO
+        if (allToolList.size() > 0) {
+            fToolNameList = new ArrayList<String>();
+            for (int i = 0; i < allToolList.size(); i++) {
+                fToolNameList.add(allToolList.get(i).getName());
+            }
+        }
+        // System.out.println("fToolNameList size is: " + fToolNameList.size());
+        // System.out.println("fToolNameList: " + fToolNameList.toString());
+    }
+
+    public void setPathofAllTools(ArrayList<String> pathList) {
+        if (pathList.size() > 0) {
+            fToolPathList = new ArrayList<String>();
+            for (int i = 0; i < pathList.size(); i++) {
+                fToolPathList.add(pathList.get(i));
+            }
+        }
+        // System.out.println("fToolPathList: " + fToolPathList.toString());
+    }
+
+    public static List<String> getToolNameList() {
+        return fToolNameList;
+    }
+
+    public static List<String> getToolPathList() {
+        return fToolPathList;
+    }
 
 }
