b0be84bd1bbc0b6b3c1680fe33756507fc9f3a67
Tobias Heck <tobias.heck@ymail.com>
2016-02-19 22:25:55 +0100

    created menu entry #168 @30m

---
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/refactoring/sorttypes/SortTypes.java
@@ -0,0 +1,10 @@
+package de.unistuttgart.iste.ps.skilled.ui.refactoring.sorttypes;
+
+public class SortTypes {
+
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.ui/src/de/unistuttgart/iste/ps/skilled/ui/refactoring/sorttypes/SortTypesHandler.java
@@ -0,0 +1,21 @@
+package de.unistuttgart.iste.ps.skilled.ui.refactoring.sorttypes;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * Activates SortTypes-routine
+ * 
+ * @author Tobias Heck
+ *
+ */
+public class SortTypesHandler extends AbstractHandler {
+    SortTypes sorter = new SortTypes();
+    
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        sorter.run();
+        return null;
+    }
+}
