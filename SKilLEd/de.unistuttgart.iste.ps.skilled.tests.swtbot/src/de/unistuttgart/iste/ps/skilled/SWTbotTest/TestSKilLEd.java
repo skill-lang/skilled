3cc3741226644af70fb4f07ee7f7e0842b787395
Jan Berberich <berberjn@studi.informatik.uni-stuttgart.de>
2016-02-22 19:25:20 +0100

    Modified Tests, added new Tests. #186

--- SKilLEd/de.unistuttgart.iste.ps.skilled.tests.swtbot/src/de/unistuttgart/iste/ps/skilled/SWTbotTest/TestSKilLEd.java
+++ SKilLEd/de.unistuttgart.iste.ps.skilled.tests.swtbot/src/de/unistuttgart/iste/ps/skilled/SWTbotTest/TestSKilLEd.java
@@ -1,6 +1,9 @@
 package de.unistuttgart.iste.ps.skilled.SWTbotTest;
 
+import java.io.File;
+
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.AfterClass;
@@ -8,33 +11,17 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import de.unistuttgart.iste.ps.skilled.tests.utils.FileLoader;
 
-//
-//import static org.eclipse.swtbot.swt.finder.SWTBotAssert.assertTextContains;
-//import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
-//import static org.eclipse.swtbot.swt.finder.waits.Conditions.tableHasRows;
-//
-//import org.eclipse.swt.custom.StyledText;
-//import org.eclipse.swt.widgets.Composite;
-//import org.eclipse.swt.widgets.Tree;
-//import org.eclipse.swt.widgets.Widget;
-//import org.eclipse.swtbot.eclipse.finder.SWTEclipseBot;
-//import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
-//import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
-//import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-//import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-//import org.eclipse.swtbot.swt.finder.utils.FileUtils;
-//import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-//import org.eclipse.swtbot.swt.finder.widgets.SWTBotStyledText;
-//import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-//import org.osgi.framework.Bundle;
-
-
-
+/**
+ * @author Jan Berberich
+ * 
+ */
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class TestSKilLEd {
 
-	private static SWTWorkbenchBot	bot;
+	private static SWTWorkbenchBot bot;
+	private String workspacePath = null;
 
 	@BeforeClass
 	public static void beforeClass() throws Exception {
@@ -43,10 +30,15 @@ public class TestSKilLEd {
 		bot.resetWorkbench();
 	}
 
+	/**
+	 * Basic test: Create a new SKilL-Project and a SKilL-File in the project and write some content in it
+	 * 
+	 */
 	@Test
-	public void testCreateSkillProject(){
+	public void testCreateSkillProject() {
 		String testProject = "TestProject";
-		//Create a new SKilL Project
+		// getWorkspacePath();
+		// Create a new SKilL Project
 		bot.menu("File").menu("New").menu("Project...").click();
 		SWTBotShell shell = bot.shell("New Project");
 		shell.activate();
@@ -54,74 +46,87 @@ public class TestSKilLEd {
 		bot.button("Next >").click();
 		bot.textWithLabel("Project name:").setText(testProject);
 		bot.button("Finish").click();
-		//assertProjectCreated(testProject);
-		
-		
-		
 		createSKilLFile("TestFile", testProject);
+		bot.viewByTitle("Project Explorer").show();
+		try {
+			bot.tree().getTreeItem(testProject).expand();
+			bot.tree().getTreeItem("TestProject").getNode("TestFile.skill");
+		} catch (WidgetNotFoundException e) {
+			throw new AssertionError(); // Project and/or File not existing->
+										// Error creating them
+		}
+		bot.tree().getTreeItem(testProject).getNode("TestFile.skill").select();
+		// Write some Text in the file
+		bot.editorByTitle("TestFile.skill").show();
+		bot.styledText().setText("A");
+		bot.styledText().setText("A{ i8 a	}");
+		bot.styledText().setText("A{i8 a; }");
+		bot.styledText().setText("A{i8 a; \n	i16 b	}");
+		bot.toolbarButtonWithTooltip("Save (Ctrl+S)").click();
+		File workspace = new File(workspacePath);
+		if (workspace.exists() && workspace.isDirectory()) {
+			for (File f : workspace.listFiles()) {
+				System.out.println("File: " + f.getName());
+			}
+		}
 		try {
 			setEditorText("TestFile", "A{}");
 		} catch (Exception e) {
 			throw new AssertionError();
 		}
-//		bot.viewByTitle("Package Explorer").setFocus();
-//		bot.tree().expandNode(testProject);
-
-		
 	}
-	
+
 	/**
-	 * This test opens a SKilL-File specified in the specification (2.3) and 
+	 * This test opens a SKilL-File specified in the specification (2.3) and
 	 * checks if it is opened in the specified time.
 	 * 
 	 */
 	@Test
-	public void openFileTimeSpecified(){
-        final long timeStart = System.currentTimeMillis(); 
-        
-        
-        
-        final long timeDiff = System.currentTimeMillis() - timeStart ; 
-        System.out.println("File opened in " + timeDiff+ " ms.");
-	}
-	
-//	
-//	  private void assertProjectCreated(String projectName) {
-//		    // Copied from #afterClass()
-//		    // FIXME: remove duplication
-//		  	
-//		    SWTBotView packageExplorerView = bot.viewByTitle("Package Explorer");
-//		    packageExplorerView.show();
-//		    Composite packageExplorerComposite = (Composite) packageExplorerView.getWidget();
-//		   
-//		    Tree swtTree = (Tree) bot.widget(new Matcher(), 0);
-//		    		
-//		    SWTBotTree tree = new SWTBotTree(swtTree);
-//		    // throws WNFE if the item does not exist
-//		    tree.getTreeItem(projectName);
-//		  }
-//	
-//	
-	
-	
-	
-	  public void setEditorText(String fileName, String text) {
-		//SWTBotEclipseEditor editor = bot.editorByTitle(fileName).toTextEditor();
-	    bot.editorByTitle(fileName).toTextEditor().setText(text);
-	    bot.editorByTitle(fileName).toTextEditor().save();
-	    
-	    //editor.setText(text);
-	    //editor.save();
+	public void openFileTimeSpecified() {
+		//First generate a File with the specified Filecontent
+		//String fileContent = FileLoader.loadFile("TestFileSpecification");
+		//System.out.println("Content: "+fileContent);
+		
+		final long timeStart = System.currentTimeMillis();
 
-	  }
-	
-	
+		final long timeDiff = System.currentTimeMillis() - timeStart;
+		System.out.println("File opened in " + timeDiff + " ms.");
+	}
+
+	public void setEditorText(String fileName, String text) {
+		// SWTBotEclipseEditor editor =
+		// bot.editorByTitle(fileName).toTextEditor();
+		bot.editorByTitle(fileName).toTextEditor().setText(text);
+		bot.editorByTitle(fileName).toTextEditor().save();
+
+		// editor.setText(text);
+		// editor.save();
+
+	}
+
+	private String getWorkspacePath() {
+		if (workspacePath == null) {
+			bot.menu("File").menu("Switch Workspace").menu("Other...").click();
+			SWTBotShell shell = bot.shell("Workspace Launcher");
+			shell.activate();
+
+			workspacePath = bot.browserWithLabel("Workspace:").getText();
+
+			System.out.println("Label: " + workspacePath);
+			bot.button("Cancel").click();
+		}
+		return workspacePath;
+	}
+
 	/**
-	 * Creates a new SKilL File with the File-> New-> Other...  Dialog.
-	 * @param fileName The name of the new SKilL-File to be created
-	 * @param projectName The name of the project the file will be in
+	 * Creates a new SKilL File with the File-> New-> Other... Dialog.
+	 * 
+	 * @param fileName
+	 *            The name of the new SKilL-File to be created
+	 * @param projectName
+	 *            The name of the project the file will be in
 	 */
-	private void createSKilLFile(String fileName, String projectName){
+	private void createSKilLFile(String fileName, String projectName) {
 		bot.menu("File").menu("New").menu("Other...").click();
 		SWTBotShell shell = bot.shell("New");
 		shell.activate();
@@ -131,34 +136,12 @@ public class TestSKilLEd {
 		SWTBotShell shell2 = bot.shell("Folder Selection");
 		shell2.activate();
 		bot.tree().select(projectName);
-	
+
 		bot.button("OK").click();
-		bot.textWithLabel("File name:").setText(fileName+".skill");
+		bot.textWithLabel("File name:").setText(fileName + ".skill");
 		bot.button("Finish").click();
-		
+
 	}
-	
-//
-//	@Test
-//	public void canCreateANewJavaProject() throws Exception {
-//		bot.menu("File").menu("New").menu("Project...").click();
-//		
-//		SWTBotShell shell = bot.shell("New Project");
-//		shell.activate();
-//		bot.tree().expandNode("Java").select("Java Project");
-//		bot.button("Next >").click();
-//
-//		bot.textWithLabel("Project name:").setText("MyFirstProject");
-//
-//		bot.button("Finish").click();
-//
-//		SWTBotShell shell2 = bot.shell("Open Associated Perspective?");
-//		
-//		shell2.activate();
-//		bot.button("No").click();
-//		// FIXME: assert that the project is actually created, for later
-//	}
-//	
 
 	@AfterClass
 	public static void sleep() {
